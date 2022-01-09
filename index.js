3029f7b3ad7a555d1f9a64e5b2f272b5293b2678
Yeohoon <yhoon.yoon@gmail.com>
2022-01-09 15:27:57 -0500

    commit tree b4a45ce0472c3244ff09f258e0dd9ee21943ce90

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
 let currentUser = " Yeohoon";
 
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 let shortGreeting = "Welcome, " + currentUser[0] + "!";
